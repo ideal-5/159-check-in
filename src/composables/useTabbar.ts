a57f1ea7f3b37f5bf767e23a3dc59dba670610d6
ideal <ide_al@163.com>
2026-01-16 14:36:20 +0800

    feat: ✨ tabbar
    tabbar配置自定义图标

--- src/composables/useTabbar.ts
+++ src/composables/useTabbar.ts
@@ -3,13 +3,15 @@ export interface TabbarItem {
   value: number | null
   active: boolean
   title: string
-  icon: string
+  // icon: string
+  activeIcon: string // 激活状态自定义图标路径
+  inactiveIcon: string // 未激活状态自定义图标路径
 }
 
 const tabbarItems = ref<TabbarItem[]>([
-  { name: 'check-in', value: null, active: true, title: '打卡', icon: 'home' },
-  { name: 'statistics', value: null, active: false, title: '统计', icon: 'statistics' },
-  { name: 'mine', value: null, active: false, title: '我的', icon: 'mine' },
+  { name: 'check-in', value: null, active: true, title: '打卡', activeIcon: '/static/tabbar/1-1.png', inactiveIcon: '/static/tabbar/1-0.png' },
+  { name: 'statistics', value: null, active: false, title: '统计', activeIcon: '/static/tabbar/2-1.png', inactiveIcon: '/static/tabbar/2-0.png' },
+  { name: 'mine', value: null, active: false, title: '我的', activeIcon: '/static/tabbar/3-1.png', inactiveIcon: '/static/tabbar/3-0.png' },
 ])
 
 export function useTabbar() {
--- src/layouts/tabbar.vue
+++ src/layouts/tabbar.vue
@@ -36,12 +36,27 @@ export default {
   <slot />
   <wd-gap safe-area-bottom height="var(--wot-tabbar-height, 50px)" />
   <wd-tabbar
-    :model-value="activeTabbar.name" bordered safe-area-inset-bottom fixed
+    :model-value="activeTabbar.name"
+    bordered
+    safe-area-inset-bottom
+    fixed
+    active-color="#010101"
+    inactive-color="#CED3DB"
     @change="handleTabbarChange"
   >
     <wd-tabbar-item
-      v-for="(item, index) in tabbarList" :key="index" :name="item.name"
-      :value="getTabbarItemValue(item.name)" :title="item.title" :icon="item.icon"
-    />
+      v-for="(item, index) in tabbarList"
+      :key="index"
+      :name="item.name"
+      :value="getTabbarItemValue(item.name)"
+      :title="item.title"
+    >
+      <template #icon="{ active }">
+        <image
+          :src="active ? item.activeIcon : item.inactiveIcon"
+          class="size-20px"
+        />
+      </template>
+    </wd-tabbar-item>
   </wd-tabbar>
 </template>
