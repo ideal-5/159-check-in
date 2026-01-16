148daae15e5df7cf51150e698c3b0da79f729af1
ideal <ide_al@163.com>
2026-01-16 14:14:58 +0800

    feat: ✨ 我的页面完成
    我的页面完成，下载图标库

--- src/App.vue
+++ src/App.vue
@@ -12,4 +12,29 @@ onLaunch(() => {})
 .wot-theme-dark.page-wraper {
   background: #222;
 }
+
+button::after {
+  border: none;
+}
+button {
+  position: relative;
+  display: block;
+  margin-left: initial;
+  margin-right: initial;
+  padding-left: initial;
+  padding-right: initial;
+  box-sizing: border-box;
+  // font-size: 18px;
+  text-align: center;
+  text-decoration: none;
+  // line-height: 1;
+  line-height: 1.35;
+  // border-radius: 5px;
+  -webkit-tap-highlight-color: transparent;
+  overflow: initial;
+  color: initial;
+  background-color: initial;
+  width: fit-content;
+  height: fit-content;
+}
 </style>
--- src/pages/mine.vue
+++ src/pages/mine.vue
@@ -2,11 +2,76 @@
 definePage({
   name: 'mine',
   layout: 'tabbar',
+  style: {
+    navigationBarTitleText: '我的',
+  },
 })
+
+const from = ref({
+  nickname: '',
+  img: '',
+  tel: '',
+})
+
+function chooseAvatar() {
+  console.log('123')
+}
 </script>
 
 <template>
-  my
+  <div class="h2.5 wf" />
+  <div class="box-border wf f-c flex-col px2.5">
+    <button
+      class="mb2.5 size-15.75 b-rd-full"
+      open-type="chooseAvatar"
+      @chooseavatar="chooseAvatar"
+    >
+      <WImage src="https://picsum.photos/200/300" custom-class="b-rd-full size-full overflow-hidden" />
+    </button>
+
+    <div class="text-(4.75 #111827) fw500">
+      温润小队长
+    </div>
+
+    <div class="mt2.5 wf b-rd-2.5 bg-#fff">
+      <div class="box-border wf flex px4 py3">
+        <div class="mr2 w-fit flex-shrink-0 text-(3.5 #111827)">
+          <span class="i-carbon:user mr2 text-primary" />
+          <span>用户名称</span>
+        </div>
+        <div class="min-w-0 flex-1">
+          <wd-input
+            v-model="from.nickname"
+            no-border
+            placeholder="请输入用户名"
+            custom-input-class="text-right!"
+            type="nickname"
+          />
+        </div>
+      </div>
+
+      <div class="box-border wf flex b-rd-2.5 bg-#fff px4 py3">
+        <div class="mr2 w-fit flex-shrink-0 text-(3.5 #111827)">
+          <span class="i-carbon:tablet mr2 text-primary" />
+          <span>手机号</span>
+        </div>
+        <div class="min-w-0 flex-1">
+          <wd-input
+            v-model="from.tel"
+            no-border
+            placeholder="请输入电话号"
+            custom-input-class="text-right!"
+            type="number"
+            :maxlength="11"
+          />
+        </div>
+      </div>
+    </div>
+
+    <wd-button custom-class="b-rd-2.5! h11! wf! mt10!">
+      退出登录
+    </wd-button>
+  </div>
 </template>
 
 <style scoped lang='scss'>
